87559261e2fae025f691c2123bc42ea079a9cb43
Christopher Snyder <snyder28139_04@hotmail.com>
2023-03-17 13:22:47 -0400

    added html/bootstrap

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+
+// var issueContainer = document.getElementById('testtest');
+
+//   var url = "https://api.openweathermap.org/data/2.5/weather?q=London,uk&units=imperial&APPID=adec8d3ec2d9ee881802e40b0d7fea07"
+
+//   fetch(url)
+// .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (data) {
+//     console.log(data.main.temp)
+//     var userName = document.createElement ('p');
+//     userName.textContent = data.main.temp;
+//     issueContainer.append(userName);
+//     }
+//   );
+
